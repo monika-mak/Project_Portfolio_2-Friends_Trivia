08d775f727cc5abd19f24044c96355ee4dcd0cff
monika mak <monika1986mak@gmail.com>
2024-05-23 15:53:28 +0000

    add trivia data file with generated questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-console.log("working!")
+//point out to where questions are stored 
+<script scr = "trivia_data"></script>;
+
+
+function username () {
+}
+
+function startQuiz () {
+}
+
+function displayQuestion () {
+}
+
+function displayHint () {
+}
+
+function checkAnswer() {
+}
+
+function displayresults () {
+}
+
+function  incrementCorrectAnswer () {
+}
+
+function  incrementCorrectAnswer () {
+}
