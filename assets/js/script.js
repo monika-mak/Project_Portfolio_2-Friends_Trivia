27b68bdda20dcc09a2dd56671f59f2ea2fbb8355
monika mak <monika1986mak@gmail.com>
2024-05-24 22:30:57 +0000

    html format,result - new butoon layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,40 +5,44 @@
 //point out to where questions are stored  
 
 
+//function createUsername () {}
 
-//function username () {}
-
+//returning  all the buttons from html onto js
+//making buttonsresponsive to click 
 document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button"); //returning  all the buttons from html onto js
+    let buttons = document.getElementsByTagName("button"); 
 
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "hint_btn") {
                 alert("you clicked hint"); //checking if function is working
             } else {
-                let user_answer = this.getAttribute("data-type");
-                alert(`you clicked ${user_answer}`);
+                let userAnswer = this.getAttribute("data-type");
+                alert(`you clicked ${userAnswer}`)
+                //startQuiz(displayQuestion);
             }
         })
     } 
 }) 
 
 
-<script scr = "trivia_data"></script>;
-function generateRandomQuestion () {
-    let triviaData = getAttribute("trivia_data");
-        for (let allQuestions of triviaData) {
-            let onlyQuestions = allQuestions[0];
-        }
-            display.(onlyQuestion).random
+//<script scr = "trivia_data"></script>;
+// //when page (DOM) finished loading - display questions
+// function startQuiz() {}
 
-            if(this.questions[0]) === "question"
-            let (thisIsQuestion)= this.question
-    }
+// function generateRandomQuestion () {
+//     let triviaData = getAttribute("trivia_data");
+//         for (let allQuestions of triviaData) {
+//             let onlyQuestions = allQuestions[0];
+//         }
+//             display.(onlyQuestion).random
+
+//             if(this.questions[0]) === "question";
+//             let (thisIsQuestion)= this.question
+//     }
 
 
-//when page (DOM) finished loading - display questions
-function displayQuestion () {
+
         
 
 //once answer is given,  
