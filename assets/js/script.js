89090aeb6c9869a60d9ce12e59e568af81b0e8fb
monika mak <monika1986mak@gmail.com>
2024-05-27 10:49:14 +0000

    change to h1 div, create next button

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,9 @@
 
 //point out to where questions are stored  
 
-
+let questionElement = document.getElementById("question");
+let answerButtons = document.getElementsByClassName("answer-buttons");
+let nextButton = document.getElementById("next")
 //function createUsername () {}
 
 //returning  all the buttons from html onto js
@@ -19,8 +21,6 @@ document.addEventListener("DOMContentLoaded", function() {
             } else {
                 let userAnswer = this.getAttribute("data-type");
                 alert(`you clicked ${userAnswer}`)
-
-//check answer 
             }
         })
     } 
@@ -30,71 +30,69 @@ document.addEventListener("DOMContentLoaded", function() {
 
 //starting with first question(index 0)
 
-let currentQuestionIndex =0;
-displayQuestion(currentQuestionIndex); 
+// let currentQuestionIndex =0;
+// displayQuestion(currentQuestionIndex); 
 
-//function to start the quiz 
-function startQuiz() {
-    currentQuestionIndex = 0;
-    displayQuestion(currentQuestionIndex);
-}
+// //function to start the quiz 
+// function startQuiz() {
+//     currentQuestionIndex = 0;
+//     displayQuestion(currentQuestionIndex);
+// }
 
-//making sure questions are within bounds
-function displayQuestion(index) {
-    if (index < 0 || index >= questions.length) {
-        console.error("Invalid question index");
-        return;
-    }
-
-
-    let questionElement = document.getElementById("displayed_question");
-    let answerButtons = document.getElementsByClassName("btn");
-}
- 
-//pull  the data from the trivia_data 
-    let questionElement = document.getElementById("displayed_question");
-    let answerButtons = document.getElementsByClassName("btn");
-
-//question text setting
-questionElement.textContent = questions[index].question;
-
-//set the answer texts 
-for (let i = 0; i < answerButtons.length; i++) {
-    answerButtons[i].textContext = question[index].answers[i];
-    answerButtons[i].setAttribute("data-type, i");
-}
-
-
-// function generateRandomQuestion () {
-//     let triviaData = getAttribute("trivia_data");
-//         for (let allQuestions of triviaData) {
-//             let presentQuestions = allQuestions;
-//         }
-//             display.(onlyQuestion).random
-
-//             if(this.questions[0]) === "question";
-//             let (thisIsQuestion)= this.question
+// //making sure questions are within bounds
+// function displayQuestion(index) {
+//     if (index < 0 || index >= questions.length) {
+//         console.error("Invalid question index");
+//         return;
 //     }
+
+  
+// }
+ 
+// //pull  the data from the trivia_data 
+//     let questionElement = document.getElementById("displayed_question");
+//     let answerButtons = document.getElementsByClassName("btn");
+
+// //question text setting
+// questionElement.textContent = questions[index].question;
+
+// //set the answer texts 
+// for (let i = 0; i < answerButtons.length; i++) {
+//     answerButtons[i].textContext = question[index].answers[i];
+//     answerButtons[i].setAttribute("data-type, i");
+// }
+
+
+// // function generateRandomQuestion () {
+// //     let triviaData = getAttribute("trivia_data");
+// //         for (let allQuestions of triviaData) {
+// //             let presentQuestions = allQuestions;
+// //         }
+// //             display.(onlyQuestion).random
+
+// //             if(this.questions[0]) === "question";
+// //             let (thisIsQuestion)= this.question
+// //     }
 
 
 
         
 
-//once answer is given,  
-function displayUserAnswer () {
-}
-//once button "hint" is pressed the hint message is displayed.
-function displayHint () {
-}
+// //once answer is given,  
+// function displayUserAnswer () {
+// }
+// //once button "hint" is pressed the hint message is displayed.
+// function displayHint () {
+// }
 
-function checkAnswer() {
-}
+// function checkAnswer() {
+// }
 
-function displayResults () {
-}
+// function displayResults () {
+// }
 
-function  incrementCorrectAnswer () {
-}
+// function  incrementCorrectAnswer () {
+// }
 
-function  incrementCorrectAnswer () {
-}
+// function  incrementCorrectAnswer () {
+// }
