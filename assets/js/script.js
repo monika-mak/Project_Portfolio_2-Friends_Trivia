014fb8010a32cb9220d5d8c0613604b850b2f7f6
monika mak <monika1986mak@gmail.com>
2024-05-28 11:03:20 +0000

    update code, check answer in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,13 @@
-//step 1. create main functions (blank)needed to run the game( for each action )
-//step 2. assign each function to its coresponding html 
-//step 3. activate functions by applying fules to each accordignally   
-
 //point out to where questions are stored  
-
-
-//pull  the data from the trivia_data 
-//function createUsername () {}
-
 //returning  all the buttons from html onto js
 //making buttons responsive to click 
+
+// Select necessary HTML elements for displaying the question and answers
+let questionElement = document.getElementById("question");
+//need to work with the full div not buttons individually
+let answerButtons = document.querySelectorAll(".answer-buttons button")
+let nextButton = document.querySelector("[data-type = 'next-btn']");
+
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
@@ -29,15 +27,6 @@ document.addEventListener("DOMContentLoaded", function () {
     startTrivia();
 });
 
-
-// Select necessary HTML elements for displaying the question and answers
-let questionElement = document.getElementById("question");
-//need to work with the full div not buttons individually
-let answerButtons = document.querySelectorAll(".answer-buttons button")
-let nextButton = document.querySelector("[data-type = 'next-btn']");
-
-
-
 //when page (DOM) finished loading - display first question
 //starting with first question(index 0) making sure it is always 0 at the start 
 
@@ -55,39 +44,46 @@ function startTrivia() {
 //function to display question 
 //questionElement = question  to be displayed 
 function showQuestion() {
-    //clear any previous answers (this is a node list needs to run through all questions) 
-    answerButtons.forEach(button => button.innerHTML = '');
-    // get current question based on current question index
+//clear any previous answers (this is a node list needs to run through all questions) 
+answerButtons.forEach(button => button.innerHTML = '');
+// get current question based on current question index
     let currentQuestion = questions[currentQuestionIndex];
-    // Display the current question number and text
+// Display the current question number and text
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
-    //loop through the answers and set the corret one 
+//loop through the answers and set the corret one 
     //set the answer texts 
     for (let i = 0; i < answerButtons.length; i++) {
         answerButtons[i].innerText = currentQuestion["choice" + (i + 1)];
         answerButtons[i].setAttribute("data-type", 'answer' + (i + 1));
     }
 }
+
 // code inspired from https://www.youtube.com/watch?v=PBcqGxrr9g8&t=356s
 function checkAnswer(userAnswer) {
-    if (userAnswer === currentQuestion.answer) {
-            selectedButton.classList.add("correct");
-        score++;
-        } else {
-            selectedButton.classList.add("incorrect");
-            };
-    showQuestion();
+    let currentQuestion = questions[currentQuestionIndex];
+    let correctAnswer = currentQuestion.answer;
 }
 
+// answerButtons.forEach(button =>  {
+//     if (button.getAttribute("data-type") === correctAnswer) {
+//         button.classList.add("correct");
+//         score++;
+//     } else if (button.getAttribute("data-type") === userAnswer) {
+//         button.classList.add("incorrect");
+//     }
+// });
+//     nextButton.style.display ="block";
+// }
 
-// //once button "hint" is pressed the hint message is displayed.
+//once button "hint" is pressed the hint message is displayed.
 // function displayHint () { 
 //     if (userAnswer === currentQuestion.hint) {
 //         hint.innerText()
 //     }
 // }
+
 
 // //once answer is given,  
 // function showNextQuestion(){ 
@@ -120,3 +116,10 @@ function checkAnswer(userAnswer) {
 // //             if(this.questions[0]) === "question";
 // //             let (thisIsQuestion)= this.question
 // //     }
+
+
+//let questionounter = 0
+//const MAX_QUESTION = 10;  - max number of questions
+
+//pull  the data from the trivia_data 
+//function createUsername () {}
