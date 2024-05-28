05a3e0be200a28b77ed7bcbb654638e367ffba00
monika mak <monika1986mak@gmail.com>
2024-05-28 07:08:08 +0000

    display questions , assign correct and incorrect attribiutes for styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,73 +16,98 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let button of buttons) {
         button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "hint-btn") {
-                alert("you clicked hint"); //checking if function is working
+                displayHint(); //checking if function is working
             } else if (this.getAttribute("data-type") === "next-btn") {
-                alert("you clicked next");
+                nextQuestion();
             } else {
                 let userAnswer = this.getAttribute("data-type");
-                alert(`you clicked ${userAnswer}`);
+                checkAnswer(userAnswer);
             }
         });
     }
+    //start the game when page loads
+    startTrivia();
 });
 
+
+// Select necessary HTML elements for displaying the question and answers
 let questionElement = document.getElementById("question");
-let answerButtons = document.getElementsByClassName("answer-buttons");
+//need to work with the full div not buttons individually
+let answerButtons = document.querySelectorAll(".answer-buttons button")
+let nextButton = document.querySelector("[data-type = 'next-btn']");
 
 
 
 //when page (DOM) finished loading - display first question
-
 //starting with first question(index 0) making sure it is always 0 at the start 
 
 let currentQuestionIndex = 0;
-let score = 0; 
+let score = 0;
 
-//function to start the quiz from 0 
+//function to start the trivia game from 0 
 function startTrivia() {
     currentQuestionIndex = 0;
-    score =0;
-    nextButton.innerHTML = "Next";
-    showQuestion();
+    score = 0; //reset score to 0;
+    nextButton.innerHTML = "Next"; // sets the text of "next" button
+    showQuestion(); //show the first question
 }
 
 //function to display question 
-//questionElement = question h1 to be displayed 
+//questionElement = question  to be displayed 
 function showQuestion() {
+    //clear any previous answers (this is a node list needs to run through all questions) 
+    answerButtons.forEach(button => button.innerHTML = '');
+    // get current question based on current question index
     let currentQuestion = questions[currentQuestionIndex];
-    let questionNo = currentQuestionIndex +1;
+    // Display the current question number and text
+    let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
-//display answers 
-    currentQuestion.answers.forEach(answer => {
-        button.innerHTML = answer.choice;
-        button.classList.add("btn")
-        answerButtons.appendChild(button);
-    }); 
+    //loop through the answers and set the corret one 
+    //set the answer texts 
+    for (let i = 0; i < answerButtons.length; i++) {
+        answerButtons[i].innerText = currentQuestion["choice" + (i + 1)];
+        answerButtons[i].setAttribute("data-type", 'answer' + (i + 1));
+    }
+}
+// code inspired from https://www.youtube.com/watch?v=PBcqGxrr9g8&t=356s
+function checkAnswer(userAnswer) {
+    if (userAnswer === currentQuestion.answer) {
+            selectedButton.classList.add("correct");
+        score++;
+        } else {
+            selectedButton.classList.add("incorrect");
+            };
+    showQuestion();
 }
 
-startTrivia();
+
+// //once button "hint" is pressed the hint message is displayed.
+// function displayHint () { 
+//     if (userAnswer === currentQuestion.hint) {
+//         hint.innerText()
+//     }
+// }
+
+// //once answer is given,  
+// function showNextQuestion(){ 
+// }
+
+// // function displayResults () {
+// // }
+
+
+
+
+
 // //making sure questions are within bounds
 // function displayQuestion(index) {
 //     if (index < 0 || index >= questions.length) {
 //         console.error("Invalid question index");
 //         return;
 //     }
-
-
 // }
 
-
-
-// //question text setting
-// questionElement.textContent = questions[index].question;
-
-// //set the answer texts 
-// for (let i = 0; i < answerButtons.length; i++) {
-//     answerButtons[i].textContext = question[index].answers[i];
-//     answerButtons[i].setAttribute("data-type, i");
-// }
 
 
 // // function generateRandomQuestion () {
@@ -95,26 +120,3 @@ startTrivia();
 // //             if(this.questions[0]) === "question";
 // //             let (thisIsQuestion)= this.question
 // //     }
-
-
-
-
-
-// //once answer is given,  
-// function displayUserAnswer () {
-// }
-// //once button "hint" is pressed the hint message is displayed.
-// function displayHint () {
-// }
-
-// function checkAnswer() {
-// }
-
-// function displayResults () {
-// }
-
-// function  incrementCorrectAnswer () {
-// }
-
-// function  incrementCorrectAnswer () {
-// }
